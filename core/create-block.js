e2492c0fb90ebc23c2d3f11bcd13e2d3e1c09fe9
lamartire <la_martire@mail.ru>
2016-08-22 18:01:21 +0300

    Create block new

--- core/create-block.js
+++ core/create-block.js
@@ -1,48 +1,117 @@
 const fs =	require('fs'),
 	  cli = require('cli-color');
 
-var arg = process.argv[2];
+let blocksDir = 'app/blocks/';
 
-if (arg == undefined) {
-	throw new Error('Block name is empty!');
+let args = process.argv,
+	parsedArgs = getArgsFromCommandLine(args);
+
+let extentableFile = parsedArgs.extFile,
+	newBlocks = parsedArgs.blocks;
+
+console.log(searchExistingBlocks(blocksDir, newBlocks))
+
+if (newBlocks.length === 0) {
+	console.log(cli.red('Here are not new blocks!'))
 } else {
-	searchExistingBlocks('app/blocks/', arg);
+
 }
 
-function searchExistingBlocks(dir, argument) {
-	fs.readdir(dir, function (err, items) {
-		items.forEach(function (item) {
-			if (item === argument) {
-				throw new Error('Block already exist!');
+/*
+ /
+ / Parse commandline text
+ /
+ */
+
+function getArgsFromCommandLine(args) {
+	let argsList = {
+		extFile: '',
+		blocks: []
+	};
+
+	for (let arg in args) {
+		if (args[arg] != process.execPath) {
+			if (args[arg] != process.mainModule.filename) {
+				if (args[arg].indexOf('EXT=') != -1) {
+					argsList.extFile = args[arg].slice(4);
+				} else {
+					argsList.blocks.push(args[arg]);
+				}
 			}
-		});
+		}
+	}
 
-		createBlock(argument, dir);
-		console.log(cli.greenBright('Block ' + cli.magentaBright(argument.toUpperCase()) + ' was successfully created!'));
-	});
+	return argsList;
 }
 
-function createBlock(blockName, targetDir) {
-	var newBlock = targetDir + blockName;
+/*
+/
+/ Search exiting block
+/
+*/
 
-	importNewBlock('app/helpers/pug/import.pug', blockName);
+function searchExistingBlocks(dir, newBlocks) {
+	let existBlocks = [];
 
-	fs.mkdir(newBlock, (err) => {
+	fs.readdir(dir, (err, items) => {
+		let dirBlocks = [];
+
 		if (err) {
-			throw err;
+			console.log(cli.red(err))
 		} else {
-			fs.writeFile(newBlock + '/' + blockName + '.pug', 'mixin ' + blockName + '()\r\n\t+b.' + blockName + '&attributes(attributes)\r\n\t\tblock', (err) => {
-				if (err) throw err;
-			});
-			fs.writeFile(newBlock + '/' + blockName + '.styl', '.' + blockName + '\r\n\tdisplay block', (err) => {
-				if (err) throw err;
+			items.map((e) => {
+				dirBlocks.push(e);
 			});
 		}
+
+		for (let i = 0; i < newBlocks.length; i++) {
+			for (let j = 0; j < dirBlocks.length; j++) {
+				if (newBlocks[i] === dirBlocks[j]) {
+					existBlocks.push(newBlocks[i]);
+				}
+			}
+		}
 	});
+
+	return existBlocks;
 }
 
-function importNewBlock(importFileName, blockName) {
-	fs.appendFile(importFileName, 'include /blocks/' + blockName + '/' + blockName + '\r\n', (err) => {
-		if (err) throw err;
-	});
-}
+
+//
+// function searchExistingBlocks(dir, argument) {
+// 	fs.readdir(dir, function (err, items) {
+// 		items.forEach(function (item) {
+// 			if (item === argument) {
+// 				throw new Error('Block already exist!');
+// 			}
+// 		});
+//
+// 		createBlock(argument, dir);
+// 		console.log(cli.greenBright('Block ' + cli.magentaBright(argument.toUpperCase()) + ' was successfully created!'));
+// 	});
+// }
+//
+// function createBlock(blockName, targetDir) {
+// 	var newBlock = targetDir + blockName;
+//
+// 	importNewBlock('app/helpers/pug/import.pug', blockName);
+//
+// 	fs.mkdir(newBlock, (err) => {
+// 		if (err) {
+// 			throw err;
+// 		} else {
+// 			fs.writeFile(newBlock + '/' + blockName + '.pug', 'mixin ' + blockName + '()\r\n\t+b.' + blockName + '&attributes(attributes)\r\n\t\tblock', (err) => {
+// 				if (err) throw err;
+// 			});
+// 			fs.writeFile(newBlock + '/' + blockName + '.styl', '.' + blockName + '\r\n\tdisplay block', (err) => {
+// 				if (err) throw err;
+// 			});
+// 		}
+// 	});
+// }
+//
+// function importNewBlock(importFileName, blockName) {
+// 	fs.appendFile(importFileName, 'include /blocks/' + blockName + '/' + blockName + '\r\n', (err) => {
+// 		if (err) throw err;
+// 	});
+// }
