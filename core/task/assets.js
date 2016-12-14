f7b34c35c84d6ad0c19c2598df2e3426c9eb7211
lamartire <la_martire@mail.ru>
2016-12-14 23:17:22 +0300

    Remove trash

--- core/task/assets.js
+++ core/task/assets.js
@@ -2,18 +2,15 @@
 
 const gulp = require('gulp'),
   browserSync = require('browser-sync'),
-  cache = require('gulp-cached'),
   imageMin = require('gulp-imagemin');
 
 gulp.task('move-fonts', () => {
   gulp.src('app/assets/fonts/**/*')
-    .pipe(cache('assets'))
     .pipe(gulp.dest('dist/assets/fonts'))
 });
 
 gulp.task('optimize-images', () => {
   gulp.src('app/assets/images/**/*')
-    .pipe(cache('assets'))
     .pipe(imageMin())
     .pipe(gulp.dest('dist/assets/images'))
     .pipe(browserSync.stream());
