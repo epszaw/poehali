d8948585cebc3b0a07ce901602fdb178a9be5055
lamartire <la_martire@mail.ru>
2016-12-14 23:08:49 +0300

    :cat: webpack hmr

--- core/task/scripts.js
+++ core/task/scripts.js
@@ -4,12 +4,12 @@ const gulp = require('gulp'),
   browserSync = require('browser-sync'),
   source = require('vinyl-source-stream'),
   fs = require('fs'),
-  webpack = require('webpack-stream'),
+  webpackStream = require('webpack-stream'),
   webpackConfig = require('../../webpack.config');
 
 gulp.task('js', () => {
   return gulp.src('app/app.js')
-    .pipe(webpack(webpackConfig))
+    .pipe(webpackStream(webpackConfig))
     .pipe(gulp.dest('dist/assets/js'))
     .pipe(browserSync.stream());
 });
